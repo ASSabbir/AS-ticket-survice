6ea6a4874833d9c335b76c3ae2df43f283fbb073
assabbir <atiksabbir1125@gmail.com>
2024-02-19 04:59:04 +0600

    again depresed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const seats =document.querySelectorAll('.seat');
 const seatContainer=document.getElementById('seat-container')
 const count=0;
+function updatePrice(){
+    const selectedSeat =document.querySelectorAll('.selectedclass');
+    const seatNumber=selectedSeat.innerText;
+    console.log(seatNumber)
+}
 seatContainer.addEventListener("click", function(value){
     if(value.target.classList.contains('seat')){
-        // value.target.classList.toggle('selected')
-        // const bgGreen=document.crecl
-        value.target.style.backgroundColor="#1DD100";
-        value.target.style.color="white";
-        count +=1;
-        console.log(count)
+        value.target.classList.toggle('selectedclass')
+        
+        updatePrice();
 
         
     }
