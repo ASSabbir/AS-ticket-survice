4393d4d1cd8241d5058bbfd8421c1a2aa9eb91d6
assabbir <atiksabbir1125@gmail.com>
2024-02-19 04:03:24 +0600

    depresed

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+
+function scrollSection() {
+    let section = document.getElementById('ticket-section');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
+const seats =document.querySelectorAll('.seat');
+const seatContainer=document.getElementById('seat-container')
+const count=0;
+seatContainer.addEventListener("click", function(value){
+    if(value.target.classList.contains('seat')){
+        // value.target.classList.toggle('selected')
+        // const bgGreen=document.crecl
+        value.target.style.backgroundColor="#1DD100";
+        value.target.style.color="white";
+        count +=1;
+        console.log(count)
 
-    function scrollSection() {
-      let section = document.getElementById('ticket-section');
-      if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-      }
+        
     }
-
+    
+})
