c012e272bd9c7810900b3b6abf683894e45807ee
assabbir <atiksabbir1125@gmail.com>
2024-02-19 19:14:16 +0600

    getting the logic

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 const seats =document.querySelectorAll('.seat');
 const seatContainer=document.getElementById('seat-container')
-const count=0;
-function updatePrice(){
-    const selectedSeat =document.querySelectorAll('.selectedclass');
-    const seatNumber=selectedSeat.innerText;
-    console.log(seatNumber)
+
+function updatePrice(i){
+    const selectedSeat=document.getElementById('selected-seat');
+    selectedSeat.innerText=i;
+    const seatLeft=document.getElementById('seat-left');
+    seatLeft.innerText=(40-i);
+    const totalPrice=document.getElementById('total-price');
+    totalPrice.innerText=550*i;
+
+    
+    
+    
 }
 seatContainer.addEventListener("click", function(value){
+    
     if(value.target.classList.contains('seat')){
-        value.target.classList.toggle('selectedclass')
+       
+        count+=1;
         
-        updatePrice();
+        if(count<=4){
+            value.target.classList.toggle('selectedclass');
+            ticketCount+=1;
+            
+            updatePrice(ticketCount);
+        }
+        else{
+            alert("You Can Buy Maximun 4 Ticket");
+            
+        }
+        
 
         
     }
